8c9c3f2050f6c966359b73a725e0846318481691
Jean Robertou <j-rob@live.fr>
2020-12-17 14:15:41 +0100

    Add accounts

---
+++ accounts.js
@@ -0,0 +1,84 @@
+const symbols = [
+	{
+		name: "pea",
+		stocks: [
+			{
+				name: "axa",
+				ticker: "CS",
+				market: "PA",
+				quantity: 5,
+				price: 17.01
+			},
+			{
+				name: "casino",
+				ticker: "CO",
+				market: "PA",
+				quantity: 4,
+				price: 23.01
+			},
+			{
+				name: "coface",
+				ticker: "COFA",
+				market: "PA",
+				quantity: 15,
+				price: 6.5
+			},
+			{
+				name: "eutelsat",
+				ticker: "ETL",
+				market: "PA",
+				quantity: 11,
+				price: 8.41
+			},
+			{
+				name: "metropole tv",
+				ticker: "MMT",
+				market: "PA",
+				quantity: 30,
+				price: 9.73
+			},
+			{
+				name: "natixis",
+				ticker: "KN",
+				market: "PA",
+				quantity: 147,
+				price: 2.04
+			},
+			{
+				name: "societe generale",
+				ticker: "GLE",
+				market: "PA",
+				quantity: 23,
+				price: 12.34
+			}
+		]
+	},
+	{
+		name: "cto",
+		stocks: [
+			{
+				name: "eutelsat",
+				ticker: "ETL",
+				market: "PA",
+				quantity: 5,
+				price: 10.24
+			},
+			{
+				name: "klepierre",
+				ticker: "LI",
+				market: "PA",
+				quantity: 5,
+				price: 17.35
+			},
+			{
+				name: "unibail rodamco westfield",
+				ticker: "URW",
+				market: "AS",
+				quantity: 4,
+				price: 46.73
+			}
+		]
+	}
+];
+
+module.exports = symbols;
