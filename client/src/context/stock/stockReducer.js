484887e109e6d460beaf4952b0dfe143d7babec3
Jean Robertou <j-rob@live.fr>
2020-12-29 09:40:26 +0100

    Fixes

--- client/src/context/stock/stockReducer.js
+++ client/src/context/stock/stockReducer.js
@@ -4,7 +4,6 @@ import { SET_STOCKS } from "../types";
 export default (state, action) => {
 	switch (action.type) {
 		case SET_STOCKS:
-			console.log("Reducer => Setting state.stocks");
 			return { ...state, stocks: action.payload };
 		default:
 			return state;
